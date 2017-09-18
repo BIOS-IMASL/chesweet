6bb75a95b8603d70bc5744a572d7bc2eeee7edf1
aloctavodia <aloctavodia@gmail.com>
2017-09-18 11:33:33 -0300

    setup draft

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='chesweet',
+      version='0.0.1',
+      description='Chemical shifts for glycans',
+      author='Pablo Garay and Osvaldo Martin',
+      author_email='garaypablo01@gmail.com, aloctavodia@gmail.com',
+      url='https://github.com/BIOS-IMASL/chesweet',
+      packages=['chesweet'],
+)
