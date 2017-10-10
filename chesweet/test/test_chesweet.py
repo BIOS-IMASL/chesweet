8c126ab631de83232e602f204e1a850611dfbaef
Pablo G Garay <garaypablo01@gmail.com>
2017-10-10 16:27:33 -0300

    first tests for compute_cs

--- chesweet/test/test_chesweet.py
+++ chesweet/test/test_chesweet.py
@@ -1,5 +1,6 @@
 #from numpy.testing import assert_almost_equal
 import numpy as np
+from ..chesweet import *
 from ..chesweet import _round_down_up, _nearest_chi
 
 torsionals = [-10, 10., 180., -180., 60., 75., -185, 185]
@@ -10,3 +11,17 @@ def test_round_down_up():
 
     for idx, t in enumerate(torsionals):
         assert _round_down_up(t, 10) == ref[idx]
+
+def test_compute_cs_all():
+    che = CheSweet(path='../lut')
+    che.compute_cs('a-D-Glcp-1-4-a-D-Glcp', 50, 60)
+
+def test_compute_cs_few():
+    che = CheSweet(path='../lut', disaccharides=['a-D-Glcp-1-1-a-D-Glcp','a-D-Glcp-1-4-a-D-Glcp','b-D-Galp-1-6-b-D-Galp'])
+    che.compute_cs('a-D-Glcp-1-1-a-D-Glcp', 50, 60)
+    che.compute_cs('a-D-Glcp-1-4-a-D-Glcp', 50, 60)
+    che.compute_cs('b-D-Galp-1-6-b-D-Galp', 50, 60, 60)
+
+def test_compute_cs_one():
+    che = CheSweet(path='../lut', disaccharides=['b-D-GlcpNAc-1-4-b-D-Glcp1Me2NAc'])
+    che.compute_cs('b-D-GlcpNAc-1-4-b-D-Glcp1Me2NAc', 50, 60)
