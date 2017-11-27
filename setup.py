08d812f1bd1879b7ef186311198aa4e1dfc3d99b
Pablo G Garay <garaypablo01@gmail.com>
2017-11-27 09:27:04 -0300

    added package data to setup

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='chesweet',
       author='Pablo Garay and Osvaldo Martin',
       author_email='garaypablo01@gmail.com, aloctavodia@gmail.com',
       url='https://github.com/BIOS-IMASL/chesweet',
+      include_package_data = True,
       packages=['chesweet'],
       install_requires=['numpy', 'scipy'],
 )
